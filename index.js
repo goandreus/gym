85a0c107c8f032ff34988fe907669a4d1e67dc4f
goandreus <andreshtml5@gmail.com>
2019-07-26 22:00:24 -0500

    kyeup

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const app = new Vue({
     el:'#app',
     data: {
-        titulo: 'Gym con vue'
-    }
+        titulo: 'Gym con vue',
+        ejercicios: [],
+        nuevoEjercicio: ''
+    },
+    methods: {
+        agregarEjercicio: function(){
+          this.ejercicios.push({
+              nombre: this.nuevoEjercicio,
+              estado: false
+          });
+            console.log(this.ejercicios)
+            this.nuevoEjercicio = ''
+        }
+    },
 })
