bfcef247ae7d5e95ce6a2366d618f9c26dcc3dcc
goandreus <andreshtml5@gmail.com>
2019-07-27 01:03:47 -0500

    local storage vue

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ const app = new Vue({
           });
             console.log(this.ejercicios)
             this.nuevoEjercicio = ''
+            localStorage.setItem('gym-vue', JSON.stringify(this.ejercicios));
         },
         editarEjercicio: function(index){
             this.ejercicios[index].estado = true;
+            localStorage.setItem('gym-vue', JSON.stringify(this.ejercicios));
+
         },
         eliminar: function(index){
             this.ejercicios.splice(index, 1);
+            localStorage.setItem('gym-vue', JSON.stringify(this.ejercicios));
+
         },
+    },
+    created:function() {
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        if(datosDB === null){
+            this.ejercicios= [];
+        }else{
+            this.ejercicios=datosDB;
+        }
     },
 })
